1b332ea3fc2ca0c2854a01fcd85838c892820213
shendingwei <48509222+shendingwei@users.noreply.github.com>
2019-07-12 07:59:59 +0800

    Add files via upload

---
+++ text/helloworld.py
@@ -0,0 +1,286 @@
+# print('==   ==  ==  ==  ==  ==  ==  ==  ==')
+# name='zhaoYi'
+# QQ=2514805337
+# tel=131456
+# cass='北京市吉利大学'
+# print('姓名：%s \nqq：%d\n手机号：%d\n手机地址：%s'%(name,QQ,tel,cass))
+# print('==   ==  ==  ==  ==  ==  ==  ==  ==')
+#
+# a=1234567890
+# b=1234567890
+# c=123456
+# d=123456
+# print('请输入银行卡号：%d\n您输入的银行卡好密码是%d\n请输入银行卡密码%d\n您输入的银行卡密码%d'%(a,b,c,d))
+
+# print('==\t==\t==\t==\t==\t==\t==\t==\t==')
+# name='zhaoyi：'
+# qq=123456789
+# tel=123456789
+# dz='北京吉利大学'
+# print('姓名：%s\nqq:%d\n电话：%d\n地址：%s'%(name,qq,tel,dz))
+#
+# print('==\t==\t==\t==\t==\t==\t==\t==\t==')
+
+
+# age = int(input('请输入自己的年龄'))
+# if age>=18:
+#     print('可以执剑走天涯了')
+# else:
+#     pass
+
+# haiba=int(input('请输入你的海拔'))
+# if haiba <= 150:
+#     print('免费进入动物园')
+# else:
+#     print('快交钱')
+
+# q=int(input('请输入你的余额'))
+# z=1
+# if q >= 2:
+#     if z == 1:
+#         print('可以上车了')
+# print('可以坐下')
+
+# s=1
+# he=0
+# while s<=100:
+#     he += s
+#     s+=1
+# print(he)
+
+# i = 1
+# while i<=5:
+#     b=1
+#         while b <=i:
+#     print('*')
+# i+=1
+
+
+# a=1
+# while a <= 9:
+#     b=1
+#     while b <= a:
+#         print('%d*%d=%d'%(b,a,b*a),end=' ')
+#         b+=1
+#     a+=1
+#     print()
+
+
+# b=1
+# while b <= 5:
+#     print('* '*b)
+#     b+=1
+
+# c=5
+# while c >=1:
+#     print('* '*c)
+#     c-=1
+
+
+# b=1
+# while b <= 9:
+#     a=1
+#     while a <= b:
+#         print('%d*%d=%d'%(a,b,b*a),end=' ')
+#         a+=1
+#     print()
+#     b+=1
+
+# b=9
+# while b >=1:
+#     a=9
+#     while a >=b:
+#         print('%d*%d=%d'%(a,b,b*a),end=' ')
+#         a-=1
+#     print()
+#     b-=1
+
+
+# a=5
+# while a >=1:
+#     print('* '*a)
+#     a-=1
+
+# for i in range(1,6):
+#     print('* '*i)
+
+# b=5
+# for c in range(b):
+#     print('* '*b)
+#     b-=1
+
+#
+# # for a in range(1,10):
+# #     for b in range(1,a+1):
+# #         print('%d*%d=%d'%(b,a,a*b),end=' ')
+#     print()
+
+# a=9
+# for c in range(1,10):
+#     b=9
+#     for b in range(1,a+1):
+#         print('%d*%d=%d'%(b,a,a*b),end=' ')
+#         b-=1
+#     print()
+#     a-=1
+
+
+# he=0
+# for s in range(66,214):
+#     he+=s
+# print(he)
+
+# i=66
+# he=0
+# while i <= 213:
+#     he+=i
+#     i += 1
+# print(he)
+
+# q=int(input('请输入年份'))
+# if q %4 == 0 and q %100 != 0 or q %400 == 0:
+#     print('是闰年')
+# else:
+#     print('是平年')
+
+# for n in range(100,2100):
+#     if n %4 ==0 and n %100 !=100 or n %400 == 0:
+#         print('%d是闰年'%n)
+#     else:
+#         print('%d是平年'%n)
+
+# n =100
+# while n <= 2099:
+#     if n %4 == 0 and n %100 != 0 or n %400==0:
+#         print('%d是闰年'%n)
+#     else:
+#         print('%d是平年'%n)
+#     n += 1
+
+
+# he=0
+# for h in range(66,214):
+#     he+=h
+# print(he)
+
+# s=66
+# he=0
+# while s<=213:
+#     he+=s
+#     s+=1
+# print(he)
+
+# n=int(input('请输入年份'))
+# if n %4 == 0 and n%100 != 0 or n %400 ==0:
+#     print('此年是闰年')
+# else:
+#     print('此年是平年')
+
+# he=0
+# for n in range(100,2099):
+#     he+=n
+# print(he)
+
+# s = 2
+# w =1
+# if s >= 2:
+#     print('可以上车')
+#     if w == 1:
+#         print('可以坐下')
+#     else:
+#         print('站着')
+# else:
+#     print('跑回家把')
+
+# wj1=int(input('输入你的猜拳：剪刀(1)石头(2)布(3)'))
+# wj2=int(input('输入你的猜拳：剪刀(1)石头(2)布(3)'))
+# if (wj1 == 1 and wj2 == 2) or(wj1 == 2 and wj2 == 3)or(wj1 == 3 and wj2 ==1):
+#     print('玩家2获胜')
+# elif wj1 == wj2:
+#     print('平局，再来一局')
+# else:
+#     print('玩家1获胜')
+
+#
+# name=[8,6,9,2,33,5]
+# name.sort()
+# print(name)
+
+# b=['shhhu','jusi','jauu']
+# b[2]='aisi'
+# print(b)
+
+# del b[1]
+# print(b)
+
+
+a_list=[2,1,4,3]
+b_list=[6,5]
+# a_list.append('666')
+# print(a_list)
+#
+# a_list.pop()
+# print(a_list)
+#
+# a_list.extend(b_list)
+# print(a_list)
+#
+# a_list[2]='8'
+# print(a_list)
+#
+# a_list.sort()
+# print(a_list)
+#
+# a_list.reverse()
+# print(a_list)
+
+
+# name='abcdef'
+# print(name[0:3:1])
+# print(name[1:3:1])
+# print(name[0:5:2])
+# print(name[-1:-6:-2])
+
+# a='abcdef'
+# print(a[:3])
+# print(a[::2])
+# print(a[1:5:2])
+# print(a[::-2])
+# print(a[5:2:-2])
+
+# my_str='hello world helld python'
+# print(my_str.split(''))
+
+# list = [1,2,3,45,85,96,74]
+# for c in list:
+#     print(c)
+
+# f= 0
+# while f < len (list):
+#     print(list[f],end=' ')
+#     f+=1
+
+# list[2]='zsj'
+# print(list)
+
+# name=['zhaowang','gjaudaiu','hjzhyusa','auuq']
+# sh=input('输入要查找的姓名')
+# if sh in name:
+#     print('找到了名字')
+# else:
+#     print('没找到')
+
+# list=[1,5,584,4,5656,88,15,54]
+# list.sort()
+# print(list)
+# list.reverse()
+# print(list)
+
+# my_str='hello world hello python'
+# print(my_str.split(' '))
+# my_str
+
+
+
+
+
